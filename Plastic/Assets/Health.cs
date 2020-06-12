96b1b6b7315f15757fa63e76958d0c1a20b1f6e6
Caramanuta <kazu_terosashi@yahoo.com>
2020-06-12 06:38:03 +0100

    Added GameOver Image once seagull hits 0 health
    The Health can no longer increase once eating a fish after hitting 0 health. The maximum health is set to 0 in that instance.

--- Plastic/Assets/Health.cs
+++ Plastic/Assets/Health.cs
@@ -8,25 +8,37 @@ public class Health : MonoBehaviour
     [SerializeField]
     Text HealthNumber;
 
-    public GameObject GameOver;
-
     int CurrentHealth = 100;
-    const int maxHealth = 100;
+    public Image GameOver;
+    //The maxHealth variable had const at the start.
+    //I have changed it to a normal int variable because when the Seagull reached 0 health
+    //The seagull could still have its health increased when eating a fish.
+    //I did not want the number to increase after it hit 0.
+    int maxHealth = 100;
+
+    public void Start()
+    {
+        //GameOver = GameObject.FindWithTag("GameOver");
+        GameOver.GetComponent<Image>().enabled = false; //GameOver image does not appear at the start of the game.
+    }
 
     public void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
         CurrentHealth = Mathf.Clamp(CurrentHealth, 0, maxHealth);
+
         if (CurrentHealth == 0)
         {
-            Death();
+            //Death();
+            maxHealth = 0;
+            GameOver.GetComponent<Image>().enabled = true;// GameOver image appears when the seagull reaches 0 health.
         }
 
         HealthNumber.text = CurrentHealth.ToString();
     }
 
-    public void Death()
+    /*public void Death()
     {
-        //GameOver.SetActive(false);
-    }
+        GameOver.layer = 2;
+    }*/
 }
